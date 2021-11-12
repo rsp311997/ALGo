cd1b69317c69d6e93e8214462ceb80cb08e4fcf3
rsp311997 <rsp311997@gmail.com>
2021-11-12 19:37:44 +0530

    Resolve Issues in Companies page GUI

---
+++ src/components/Basic/GridImage.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "../../style/GridImage.css";
+
+class GridImage extends React.Component{
+    render(){
+        return(
+            <div className="mt-5">
+                            {this.props.obj.map((obj,index)=>{
+                                return (
+                                    <div key={index} className="wrapper">
+                                            <div className="wrapper-text">
+                                                <div style={{width:"100%",height:"33%",paddingTop:"10px"}}>
+                                                    <div style={{position:"absolute",right:"0",backgroundColor:"red"}}>
+                                                        {obj.status}
+                                                    </div>
+                                                </div>
+                                                <div style={{width:"100%",height:"100%",paddingLeft:"30px",paddingRight:"5px"}}>
+                                                    <div>
+                                                        {obj.title}
+                                                    </div>
+                                                    <div>
+                                                        {obj.description}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="wrapper-overlay">
+
+                                            </div>
+                                            <img className="wrapper-img" src={obj.IMG_URL} alt="companies" />
+                                    </div>
+                                );
+                            })}
+                    </div>
+        );
+    }
+}
+
+export default GridImage;
--- src/components/CompaniesPage/CompaniesPage.js
+++ src/components/CompaniesPage/CompaniesPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBar from "../Basic/NavBar.js";
 import Footer from "../Basic/Footer.js";
+import GridImage from "../Basic/GridImage.js";
 
 import "../../style/NavBar.css";
 import "../../style/CompaniesPage.css";
@@ -23,19 +24,19 @@ class CompaniesPage extends React.Component{
                     status:"accuaired"
                 },
                 {
-                    IMG_URL:process.env.PUBLIC_URL+"/media/images/rec2.png",
+                    IMG_URL:process.env.PUBLIC_URL+"/media/images/rec1.png",
                     title:"LOGO",
                     description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
                     status:"accuaired"
                 },
                 {
-                    IMG_URL:process.env.PUBLIC_URL+"/media/images/rec3.png",
+                    IMG_URL:process.env.PUBLIC_URL+"/media/images/rec1.png",
                     title:"LOGO",
                     description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
                     status:"accuaired"
                 },
                 {
-                    IMG_URL:process.env.PUBLIC_URL+"/media/images/rec5.png",
+                    IMG_URL:process.env.PUBLIC_URL+"/media/images/rec1.png",
                     title:"LOGO",
                     description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."
                 },
@@ -45,17 +46,17 @@ class CompaniesPage extends React.Component{
                     description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."
                 },
                 {
-                    IMG_URL:process.env.PUBLIC_URL+"/media/images/rec2.png",
+                    IMG_URL:process.env.PUBLIC_URL+"/media/images/rec1.png",
                     title:"LOGO",
                     description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."
                 },
                 {
-                    IMG_URL:process.env.PUBLIC_URL+"/media/images/rec3.png",
+                    IMG_URL:process.env.PUBLIC_URL+"/media/images/rec1.png",
                     title:"LOGO",
                     description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."
                 },
                 {
-                    IMG_URL:process.env.PUBLIC_URL+"/media/images/rec5.png",
+                    IMG_URL:process.env.PUBLIC_URL+"/media/images/rec1.png",
                     title:"LOGO",
                     description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."
                 },
@@ -87,41 +88,20 @@ class CompaniesPage extends React.Component{
                     <div className="companies-section2">
                         <div className="row mt-5 mb-2">
                             <div className="col-6 text-center">
-                                <a href="#" className="companies-section2-anchor-prop-disable">Status <span className="companies-section2-arrow-head-down">></span></a>
+                                <a href="/companies/" className="companies-section2-anchor-prop-disable">Status <span className="companies-section2-arrow-head-down">></span></a>
                             </div>
                             <div className="col-6 text-center">
-                                <a href="#" className="companies-section2-anchor-prop-disable">Indusrty <span className="companies-section2-arrow-head-down">></span></a>
+                                <a href="/companies/" className="companies-section2-anchor-prop-disable">Indusrty <span className="companies-section2-arrow-head-down">></span></a>
                             </div>
                         </div>
                     </div>
                 {/* Section 2 */}
 
                 {/* Section 3 */}
-                    <ul className="row list-group list-group-horizontal justify-content-center mt-5" style={{width:"100%",paddingLeft:"0px",marginLeft:"0px"}}>
-                        {this.state.companies.map((obj,index)=>{
-                            return (
-                                <li key={index} className="list-group-item companies-section3-list-group-item" style={{padding:"0px"}}>
-                                    <div class="companies-section3-wrapper">
-                                        <div className="companies-section3-wrapper-text">
-                                            <div className="row">
-                                                <div className="col-12 mt-5 text-right">
-                                                    <span style={{backgroundColor:"red"}}>Acquired</span>
-                                                </div>
-                                                <div className="col-12 mt-5 pt-5">
-                                                    LOGO
-                                                </div>
-                                                <div className="col-12 mt-3">
-                                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <img className="companies-section3-wrapper-img" src={obj.IMG_URL} alt="rec"/>
-                                    </div>
-                                </li>
-                            );
-                        })}
-                    </ul>
+                    <GridImage obj={this.state.companies}/>
                 {/* Section 3 */}
+
+                
 
                 {/* Section -1 */}
                     <Footer />
